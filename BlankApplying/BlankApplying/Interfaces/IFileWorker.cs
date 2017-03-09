298c87e9a39b5883a0acbb443ee4895d18986f96
Valerii Rotermel <rotermel.valera@gmail.com>
2017-03-09 21:41:25 +0200

    Added new interface, created new branch. Commiting chagnes for having more commits.

---
+++ BlankApplying/BlankApplying/Interfaces/IFileWorker.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BlankApplying.Interfaces
+{
+    public interface IFileWorker
+    {
+        Task<bool> ExistAsync(string fileName);
+        Task SaveTextAsync(string fileName, string text);
+        Task<string> LoadTextAsync(string fileName);
+        Task<IEnumerable<string>> GeFileAsync();
+        Task DeleteAsync(string fileName);
+    }
+}
